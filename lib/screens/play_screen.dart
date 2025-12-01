682a0a34fd7b4a16814ec399a9b680eac2658af5
arinash <arina.shumeljuk@gmail.com>
2025-12-01 19:35:08 +0100

    Chnaged to ai scanner

--- lib/screens/play_screen.dart
+++ lib/screens/play_screen.dart
@@ -1,10 +1,6 @@
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-
-// Import both packages normally
-import 'package:mobile_scanner/mobile_scanner.dart';
-import 'package:qr_code_dart_scan/qr_code_dart_scan.dart';
+import 'package:ai_barcode_scanner/ai_barcode_scanner.dart';
 
 class PlayScreen extends StatefulWidget {
   const PlayScreen({super.key});
@@ -19,7 +15,6 @@ class _PlayScreenState extends State<PlayScreen> {
   Future<void> _openLink(String url) async {
     try {
       final Uri incoming = Uri.parse(url);
-
       String? spotifyAppUri;
       Uri webUri = incoming;
 
@@ -60,19 +55,6 @@ class _PlayScreenState extends State<PlayScreen> {
     }
   }
 
-  void _handleScan(String code) {
-    if (_hasScanned) return;
-    
-    setState(() {
-      _hasScanned = true;
-    });
-
-    debugPrint('QR code detected: $code');
-    _openLink(code);
-    
-    Navigator.pop(context);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -85,85 +67,39 @@ class _PlayScreenState extends State<PlayScreen> {
         centerTitle: true,
         foregroundColor: Colors.white,
       ),
-      body: kIsWeb 
-          ? _WebScanner(onScan: _handleScan, hasScanned: _hasScanned)
-          : _MobileScanner(onScan: _handleScan, hasScanned: _hasScanned),
-    );
-  }
-}
+      body: AiBarcodeScanner(
+        onDetect: (BarcodeCapture capture) {
+          if (_hasScanned) return;
+          
+          final barcode = capture.barcodes.firstOrNull;
+          if (barcode == null || barcode.rawValue == null) return;
+          
+          setState(() {
+            _hasScanned = true;
+          });
 
-class _MobileScanner extends StatefulWidget {
-  final Function(String) onScan;
-  final bool hasScanned;
-
-  const _MobileScanner({required this.onScan, required this.hasScanned});
-
-  @override
-  State<_MobileScanner> createState() => _MobileScannerState();
-}
-
-class _MobileScannerState extends State<_MobileScanner> {
-  late final MobileScannerController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = MobileScannerController(
-      detectionSpeed: DetectionSpeed.normal,
-      facing: CameraFacing.back,
-    );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        MobileScanner(
-          controller: _controller,
-          onDetect: (barcodeCapture) {
-            final String? code = barcodeCapture.barcodes.first.rawValue;
-            if (code != null && !widget.hasScanned) {
-              _controller.stop();
-              widget.onScan(code);
-            }
-          },
+          final String code = barcode.rawValue!;
+          debugPrint('QR code detected: $code');
+          _openLink(code);
+          
+          Navigator.pop(context);
+        },
+        controller: MobileScannerController(
+          detectionSpeed: DetectionSpeed.noDuplicates,
         ),
-        Center(
-          child: Container(
-            width: 250,
-            height: 250,
-            decoration: BoxDecoration(
-              border: Border.all(color: const Color(0xFF2B5FC7), width: 2),
-              borderRadius: BorderRadius.circular(10)
+        overlayBuilder: (context, constraints) {
+          return Center(
+            child: Container(
+              width: 250,
+              height: 250,
+              decoration: BoxDecoration(
+                border: Border.all(color: const Color(0xFF2B5FC7), width: 2),
+                borderRadius: BorderRadius.circular(10)
+              ),
             ),
-          ),
-        )
-      ],
-    );
-  }
-}
-
-class _WebScanner extends StatelessWidget {
-  final Function(String) onScan;
-  final bool hasScanned;
-
-  const _WebScanner({required this.onScan, required this.hasScanned});
-
-  @override
-  Widget build(BuildContext context) {
-    return QRCodeDartScanView(
-      scanInvertedQRCode: true,
-      onCapture: (Result result) {
-        if (!hasScanned) {
-          onScan(result.text);
-        }
-      },
+          );
+        },
+      ),
     );
   }
 }
